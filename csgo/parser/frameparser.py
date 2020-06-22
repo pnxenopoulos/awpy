563ab27845c456e98d64b855d3ccacd80b26243a
pnxenopoulos <pnxenopoulos@gmail.com>
2020-06-21 22:59:50 -0400

    referenced xml tree correctly in frameparser

--- csgo/parser/frameparser.py
+++ csgo/parser/frameparser.py
@@ -109,7 +109,7 @@ class FrameParser:
         for j, round_elem in enumerate(self.game):
             if start_round < j:
                 self.game.remove(round_elem)
-        tree.write(open(self.match_id + ".xml", "w"), encoding="unicode")
+        self.tree.write(open(self.match_id + ".xml", "w"), encoding="unicode")
         self.logger.info("Cleaned the round XML to remove noisy rounds")
 
     def parse(self, df=True):
