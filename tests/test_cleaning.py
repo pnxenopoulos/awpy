5211761d3a8ef8f6f2c5529b78c2272a894b187c
pnxenopoulos <pnxenopoulos@gmail.com>
2020-07-02 15:08:31 -0400

    Added association function

--- tests/test_cleaning.py
+++ tests/test_cleaning.py
@@ -1,24 +1,32 @@
 import pytest
 
-from csgo.parser.cleaning import associate_players
+from csgo.parser.cleaning import associate_entities
 
 
 class TestCleaning:
     """ Class to test CSGO data cleaning functions
     """
 
-    def test_player_association(self):
-        """ Test player association
+    def test_association(self):
+        """ Test entity association
         """
         a = ["misutaaa-", "ZyW0o//", "peeter"]
         b = ["misuta", "Zywoo", "peter"]
-        c = associate_players(a, b)
+        c = associate_entities(a, b)
         assert c["misutaaa-"] == "misuta"
 
-    def test_player_association_length(self):
-        """ Tests associate players function errors on lists on unequal length.
+    def test_association_length(self):
+        """ Tests association function errors on lists on unequal length.
         """
         a = ["misutaaa-", "ZyW0o//"]
         b = ["misuta", "Zywoo", "peter"]
         with pytest.raises(ValueError):
-            associate_players(a, b)
+            associate_entities(a, b)
+
+    def test_wrong_metric(self):
+        """ Tests if submitting a wrong metric raises an error. 
+        """
+        a = ["misutaaa-", "ZyW0o//"]
+        b = ["misuta", "Zywoo", "peter"]
+        with pytest.raises(ValueError):
+            associate_entities(a, b, metric="bad_metric")
