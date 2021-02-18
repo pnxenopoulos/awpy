e8beafa5916e4ba094e373723e39759fbc09c2ca
pnxenopoulos <pnxenopoulos@gmail.com>
2021-02-18 11:42:34 -0500

    fixed errant paren

--- csgo/parser/parse_demo.go
+++ csgo/parser/parse_demo.go
@@ -738,13 +738,10 @@ func main() {
 		gs := p.GameState()
 
 		if (acceptableGamePhase(gs)) {
-			if convParsed == false {)
+			if convParsed == false {
 				// If convars are unparsed, record the convars of the server
 				serverConfig := ServerConVar{}
 				conv := gs.ConVars()
-				for key, value := range conv {
-					fmt.Println("Key:", key, "Value:", value)
-				}
 				serverConfig.CashBombDefused, _ = strconv.ParseInt(conv["cash_player_bomb_defused"], 10, 64)
 				serverConfig.CashBombPlanted, _ = strconv.ParseInt(conv["cash_player_bomb_planted"], 10, 64)
 				serverConfig.CashWinBomb, _ = strconv.ParseInt(conv["cash_team_terrorist_win_bomb"], 10, 64)
