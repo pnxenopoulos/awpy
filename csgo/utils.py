13624fe9aa4591ed84c1089f3f6241506f6b6436
arjun-22 <56309648+arjun-22@users.noreply.github.com>
2021-08-11 20:04:46 -0400

    Added damage events aggregation function

--- csgo/utils.py
+++ csgo/utils.py
@@ -50,3 +50,11 @@ def check_go_version():
     except Exception as e:
         print(e)
         return False
+    
+
+def agg_damages(damage_data: pd.DataFrame) -> pd.DataFrame:
+    """Returns a dataframe with aggregated damage events."""
+    cols_to_groupby = list(damage_data.columns[0:25])
+    cols_to_groupby.extend(list(damage_data.columns[29:]))
+    damage_data = damage_data.groupby(cols_to_groupby).sum().reset_index()
+    return damage_data
