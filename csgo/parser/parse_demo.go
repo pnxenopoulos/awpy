060894cafb03addaedd6d8ff19f9ba73ed9ebf5c
pnxenopoulos <pnxenopoulos@gmail.com>
2021-08-20 18:03:24 -0400

    testing flashthrower

--- csgo/parser/parse_demo.go
+++ csgo/parser/parse_demo.go
@@ -1321,7 +1321,8 @@ func main() {
 			if e.Victim.IsBlinded() {
 				// Find their latest flash event
 				for _, flash := range currentRound.Flashes {
-					if (flash.PlayerSteamID == currentKill.VictimSteamID) && (flash.Tick >= currentKill.Tick - 2*currentGame.TickRate) && (flash.Tick <= currentKill.Tick) {
+					if (flash.PlayerSteamID == currentKill.VictimSteamID) {
+						// && (flash.Tick >= currentKill.Tick - 2*currentGame.TickRate) && (flash.Tick <= currentKill.Tick)
 						currentKill.FlashThrowerSteamID = &flash.AttackerSteamID
 						currentKill.FlashThrowerName = &flash.AttackerName
 						currentKill.FlashThrowerTeam = &flash.AttackerTeam
