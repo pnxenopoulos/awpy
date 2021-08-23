bc89790b281822eeaab325bf3c0d4a1137eccd6a
pnxenopoulos <pnxenopoulos@gmail.com>
2021-08-23 14:30:33 -0400

    changing flash duration method

--- csgo/parser/parse_demo.go
+++ csgo/parser/parse_demo.go
@@ -1178,7 +1178,7 @@ func main() {
 				currentFlash.PlayerViewY = &playerViewY
 
 				// Calculate flash duration
-				flashDuration := float64(e.Player.FlashDuration()/1000)
+				flashDuration := float64(e.Player.FlashDurationTime()/1000)
 				currentFlash.FlashDuration = &flashDuration
 
 				// Add
