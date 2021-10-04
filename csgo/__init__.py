b3cf98caf57e4a10af014c833bb4a26bc2f4c9f2
pnxenopoulos <pnxenopoulos@gmail.com>
2021-10-04 10:07:27 -0400

    removing old cols, adding pathlib to startup data

--- csgo/__init__.py
+++ csgo/__init__.py
@@ -4,6 +4,8 @@ import os
 import pickle
 import pandas as pd
 
+from pathlib import Path
+
 # import brotli
 
 path = os.path.join(os.path.dirname(__file__), "")
@@ -11,5 +13,5 @@ path = os.path.join(os.path.dirname(__file__), "")
 DIST = pickle.load(lzma.open(path + "data/nav/distances.xz", "rb"))
 MAP_NAV = pd.read_csv(path + "data/nav/map_nav.csv")
 
-with open(path + "data/map/map_data.json") as f:
+with open(Path(path + "data/map/map_data.json")) as f:
     MAP_DATA = json.load(f)
