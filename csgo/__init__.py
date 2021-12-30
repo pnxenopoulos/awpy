1e64ed31af97d310c30065ca53def5ebaac4a747
pnxenopoulos <pnxenopoulos@gmail.com>
2021-12-30 01:11:42 -0800

    removing old files

--- csgo/__init__.py
+++ csgo/__init__.py
@@ -10,7 +10,6 @@ from pathlib import Path
 
 path = os.path.join(os.path.dirname(__file__), "")
 
-DIST = pickle.load(lzma.open(path + "data/nav/distances.xz", "rb"))
 MAP_NAV = pd.read_csv(path + "data/nav/map_nav.csv")
 
 with open(Path(path + "data/map/map_data.json")) as f:
---
+++ tests/test_nav.py
@@ -0,0 +1,6 @@
+class TestNav:
+    """Class to test the nav-related functions."""
+
+    def test_nav(self):
+        """Tests nav"""
+        assert True == True
