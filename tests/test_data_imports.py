56362230516c9000dfb242adeac3468084396168
pnxenopoulos <pnxenopoulos@gmail.com>
2021-12-30 10:10:12 -0800

    fixing nav csv

--- tests/test_data_imports.py
+++ tests/test_data_imports.py
@@ -2,16 +2,16 @@ import os
 import pytest
 import networkx
 
-from csgo import MAP_NAV, MAP_DATA, NAV, NAV_GRAPHS
+from csgo import MAP_DATA, NAV, NAV_CSV, NAV_GRAPHS
 
 
 class TestDataImports:
     """Class to test the data imports"""
 
-    def test_map_nav(self):
+    def test_nav_csv(self):
         """Tests the nav dataframe"""
-        assert MAP_NAV[MAP_NAV["MapName"] == "de_cbble"].shape[0] == 1180
-        assert MAP_NAV.isna().sum().sum() == 0
+        assert NAV_CSV[NAV_CSV["MapName"] == "de_cbble"].shape[0] == 1180
+        assert NAV_CSV.isna().sum().sum() == 0
 
     def test_nav(self):
         assert type(NAV) == dict
