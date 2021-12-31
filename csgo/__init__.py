fe2c0ad12bde66d05b40689a5fca7334f3ede4a8
pnxenopoulos <pnxenopoulos@gmail.com>
2021-12-31 11:17:26 -0800

    adding test prints

--- csgo/__init__.py
+++ csgo/__init__.py
@@ -12,16 +12,17 @@ path = os.path.join(os.path.dirname(__file__), "")
 # Create nav tile info
 nav_dfs = []
 for file in os.listdir(path + "data/nav/"):
-    if "de_dust2" in file:
-        df = pd.read_csv(path + "data/nav/" + file)
-        print(df.columns)
-        #print(df[df["areaId"] == 152])
     if file.endswith(".csv"):
         df = pd.read_csv(path + "data/nav/" + file)
+        print("-----")
+        print(df.columns)
+        print("-----")
         nav_dfs.append(df)
 NAV_CSV = pd.concat(nav_dfs)
 NAV_CSV.areaName = NAV_CSV.areaName.fillna("")
-#print(NAV_CSV[NAV_CSV["areaId"] == 152])
+print("-----")
+print(NAV_CSV.columns)
+print("-----")
 NAV = transform_csv_to_json(NAV_CSV)
 print(NAV["de_dust2"][152])
 
