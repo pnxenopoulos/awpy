0bdf216111696eb40bf551dd8cc157d9ea2b0f7d
pnxenopoulos <pnxenopoulos@gmail.com>
2022-01-12 17:25:14 -0500

    fixing warmups test

--- tests/test_demo_parse.py
+++ tests/test_demo_parse.py
@@ -261,7 +261,7 @@ class TestDemoParser:
         assert len(self.warmup_data["gameRounds"]) == 30
         self._check_round_scores(self.warmup_data["gameRounds"])
         self.warmup_parser_sneem = DemoParser(
-            demofile="vitality-vs-g2-m2-mirage.dem", log=False, parse_frames=False
+            demofile="vitality-vs-g2-m2-mirage.dem", log=False, parse_frames=True
         )
         self.warmup_sneem_data = self.warmup_parser_sneem.parse()
         self.warmup_sneem_data = self.warmup_parser_sneem.clean_rounds(
