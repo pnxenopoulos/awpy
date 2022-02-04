17ede0d117693a9fc3438e3b7a94607573fba4e9
Peter Xenopoulos <42321130+pnxenopoulos@users.noreply.github.com>
2022-02-03 19:45:40 -0500

    updating version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="awpy",
-    version="1.0",
+    version="1.0.1",
     packages=find_packages(),
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
