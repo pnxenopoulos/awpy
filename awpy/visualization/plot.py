2db43f429e528e192834049203e650d267576c1a
Jan-Eric Nitschke <jan-eric@nitschke.tk>
2022-06-25 13:19:36 +0200

    Added function to plots multiple rounds and group players by proximity.

--- awpy/visualization/plot.py
+++ awpy/visualization/plot.py
@@ -1,3 +1,4 @@
+from email.policy import default
 import os
 import shutil
 import imageio
@@ -91,10 +92,6 @@ def plot_positions(
     Returns:
         matplotlib fig and ax
     """
-    logging.info(alphas)
-    logging.info(positions)
-    logging.info(markers)
-    logging.info(sizes)
     f, a = plot_map(map_name=map_name, map_type=map_type, dark=dark)
     for p, c, m, al, s in zip(positions, colors, markers, alphas, sizes):
         if apply_transformation:
@@ -200,10 +197,38 @@ def get_player_id(player):
     if player["steamID"] == 0:
         return player["name"]
     else:
-        return player["steamID"]
+        return str(player["steamID"])
 
 
-def plot_rounds(
+def get_closest_leader(leaders, assigned, pos):
+    """Get closest leader"""
+    logging.info("Number of existing leaders: %s", len(leaders))
+    logging.info("Already assigned leaders: %s", assigned)
+    logging.info("my pos: %s", pos)
+    closest_leader = None
+    smallest_dist = float("inf")
+    for leader in leaders:
+        if leader in assigned:
+            continue
+        dist = (pos[0] - leaders[leader]["pos"][0]) ** 2 + (
+            pos[1] - leaders[leader]["pos"][1]
+        ) ** 2
+        if dist < smallest_dist:
+            smallest_dist = dist
+            closest_leader = leader
+    return closest_leader
+
+
+def tree():
+    """Build tree structure"""
+
+    def the_tree():
+        return collections.defaultdict(the_tree)
+
+    return the_tree()
+
+
+def plot_rounds_different_players(
     filename, frames_list, map_name="de_ancient", map_type="original", dark=False
 ):
     """Plots a round and saves as a .gif. CTs are blue, Ts are orange, and the bomb is an octagon. Only use untransformed coordinates.
@@ -224,7 +249,6 @@ def plot_rounds(
     os.mkdir("csgo_tmp")
     image_files = []
     dict_initialized = {"t": False, "ct": False}
-    id_number_dict = {"t": {}, "ct": {}}
     frame_positions = collections.defaultdict(list)
     frame_colors = collections.defaultdict(list)
     frame_markers = collections.defaultdict(list)
@@ -234,18 +258,22 @@ def plot_rounds(
     # logging.info(type(frames_list))
     # logging.info([frames[0]["tick"] for frames in frames_list])
     max_frames = max(len(frames) for frames in frames_list)
+    leaders = tree()
+    sides = ["ct"]
     # logging.info(max_frames)
     for i in tqdm(range(max_frames)):
+        # logging.info(leaders)
         # for i in range(max_frames):
         positions = []
         colors = []
         markers = []
         alphas = []
         sizes = []
-        first_alive = [-1] * 5
+        checked_in = set()
         for frame_index, frames in enumerate(frames_list):
             # logging.info(type(frames))
             # logging.info(frame_index, frames[0])
+            assigned = set()
             frame_positions[frame_index] = []
             frame_colors[frame_index] = []
             frame_markers[frame_index] = []
@@ -253,44 +281,68 @@ def plot_rounds(
             frame_sizes[frame_index] = []
             if i in range(len(frames)):
                 # Plot players
-                for side in ["ct"]:
+                for side in sides:
                     for player_index, p in enumerate(frames[i][side]["players"]):
-                        player_id = get_player_id(p)
-                        if dict_initialized[side] is False:
-                            id_number_dict[side][player_id] = player_index
-                        if player_id not in id_number_dict[side]:
-                            continue
-                        frame_colors[frame_index].append(
-                            colors_list[id_number_dict[side][player_id]]
-                        )
-                        if p["hp"] == 0:
-                            frame_markers[frame_index].append("x")
-                        else:
-                            frame_markers[frame_index].append(
-                                r"$ {} $".format(frame_index)
-                            )
-                            if first_alive[id_number_dict[side][player_id]] == -1:
-                                first_alive[
-                                    id_number_dict[side][player_id]
-                                ] = frame_index
-                            # frame_markers[frame_index].append(".")
+                        player_id = get_player_id(p) + "_" + str(frame_index)
                         pos = (
                             position_transform(map_name, p["x"], "x"),
                             position_transform(map_name, p["y"], "y"),
                         )
+
+                        is_dead = False
+                        if p["hp"] == 0:
+                            is_dead = True
+                            frame_markers[frame_index].append("x")
+                        else:
+                            frame_markers[frame_index].append(".")
+
+                        if dict_initialized[side] is False:
+                            leaders[side][player_id]["index"] = player_index
+                            leaders[side][player_id]["pos"] = pos
+
+                        if player_id in leaders[side] and player_id not in checked_in:
+                            if is_dead:
+                                leaders[side][player_id]["is_dead"] = True
+                            player_index = leaders[side][player_id]["index"]
+                            leaders[side][player_id]["pos"] = pos
+
+                        else:
+                            closest_leader = get_closest_leader(
+                                leaders[side], assigned, pos
+                            )
+                            if (
+                                leaders[side][closest_leader]["is_dead"]
+                                or closest_leader not in checked_in
+                            ) and not is_dead:
+                                old_index = leaders[side][closest_leader]["index"]
+                                del leaders[side][closest_leader]
+                                leaders[side][player_id]["index"] = old_index
+                                leaders[side][player_id]["pos"] = pos
+                                leaders[side][player_id]["is_dead"] = False
+                                player_index = leaders[side][player_id]["index"]
+                                assigned.add(player_id)
+                            else:
+                                player_index = leaders[side][closest_leader]["index"]
+                                if not is_dead:
+                                    assigned.add(closest_leader)
+                        frame_colors[frame_index].append(colors_list[player_index])
                         frame_positions[frame_index].append(pos)
-                        frame_alphas[frame_index].append(
-                            1
-                            if frame_index
-                            == first_alive[id_number_dict[side][player_id]]
-                            else 0.2
-                        )
-                        frame_sizes[frame_index].append(
-                            mpl.rcParams["lines.markersize"] ** 2
-                            if frame_index
-                            == first_alive[id_number_dict[side][player_id]]
-                            else 0.25 * mpl.rcParams["lines.markersize"] ** 2
-                        )
+                        if (
+                            player_id in leaders[side]
+                            and not leaders[side][player_id]["is_dead"]
+                            and not player_id in checked_in
+                        ):
+                            frame_alphas[frame_index].append(1)
+                            frame_sizes[frame_index].append(
+                                mpl.rcParams["lines.markersize"] ** 2
+                            )
+                        else:
+                            frame_alphas[frame_index].append(0.2)
+                            frame_sizes[frame_index].append(
+                                0.25 * mpl.rcParams["lines.markersize"] ** 2
+                            )
+                        if player_id in leaders[side]:
+                            checked_in.add(player_id)
                     dict_initialized[side] = True
             positions.extend(frame_positions[frame_index])
             colors.extend(frame_colors[frame_index])
@@ -365,3 +417,104 @@ def plot_nades(
     a.get_xaxis().set_visible(False)
     a.get_yaxis().set_visible(False)
     return f, a
+
+
+def plot_rounds_same_players(
+    filename, frames_list, map_name="de_ancient", map_type="original", dark=False
+):
+    """Plots a round and saves as a .gif. CTs are blue, Ts are orange, and the bomb is an octagon. Only use untransformed coordinates.
+
+    Args:
+        filename (string): Filename to save the gif
+        frames_list (list): List of frames lists from parsed demos
+        markers (list): List of marker types for each player
+        map_name (string): Map to search
+        map_type (string): "original" or "simpleradar"
+        dark (boolean): Only for use with map_type="simpleradar". Indicates if you want to use the SimpleRadar dark map type
+
+    Returns:
+        matplotlib fig and ax, saves .gif
+    """
+    if os.path.isdir("csgo_tmp"):
+        shutil.rmtree("csgo_tmp/")
+    os.mkdir("csgo_tmp")
+    image_files = []
+    dict_initialized = {"t": False, "ct": False}
+    id_number_dict = {"t": {}, "ct": {}}
+    frame_positions = collections.defaultdict(list)
+    frame_colors = collections.defaultdict(list)
+    frame_markers = collections.defaultdict(list)
+    frame_alphas = collections.defaultdict(list)
+    frame_sizes = collections.defaultdict(list)
+    colors_list = ["cyan", "red", "green", "blue", "orange"]
+    max_frames = max(len(frames) for frames in frames_list)
+    for i in tqdm(range(max_frames)):
+        positions = []
+        colors = []
+        markers = []
+        alphas = []
+        sizes = []
+        first_alive = [-1] * 5
+        for frame_index, frames in enumerate(frames_list):
+            frame_positions[frame_index] = []
+            frame_colors[frame_index] = []
+            frame_markers[frame_index] = []
+            frame_alphas[frame_index] = []
+            frame_sizes[frame_index] = []
+            if i in range(len(frames)):
+                # Plot players
+                for side in ["ct"]:
+                    for player_index, p in enumerate(frames[i][side]["players"]):
+                        player_id = get_player_id(p)
+                        if dict_initialized[side] is False:
+                            id_number_dict[side][player_id] = player_index
+                        player_index = id_number_dict[side][player_id]
+                        if player_id not in id_number_dict[side]:
+                            continue
+                        frame_colors[frame_index].append(colors_list[player_index])
+                        if p["hp"] == 0:
+                            frame_markers[frame_index].append("x")
+                        else:
+                            frame_markers[frame_index].append(
+                                r"$ {} $".format(frame_index)
+                            )
+                            if first_alive[player_index] == -1:
+                                first_alive[player_index] = frame_index
+                        pos = (
+                            position_transform(map_name, p["x"], "x"),
+                            position_transform(map_name, p["y"], "y"),
+                        )
+                        frame_positions[frame_index].append(pos)
+                        frame_alphas[frame_index].append(
+                            1 if frame_index == first_alive[player_index] else 0.2
+                        )
+                        frame_sizes[frame_index].append(
+                            mpl.rcParams["lines.markersize"] ** 2
+                            if frame_index == first_alive[player_index]
+                            else 0.25 * mpl.rcParams["lines.markersize"] ** 2
+                        )
+                    dict_initialized[side] = True
+            positions.extend(frame_positions[frame_index])
+            colors.extend(frame_colors[frame_index])
+            markers.extend(frame_markers[frame_index])
+            alphas.extend(frame_alphas[frame_index])
+            sizes.extend(frame_sizes[frame_index])
+        f, a = plot_positions(
+            positions=positions,
+            colors=colors,
+            markers=markers,
+            alphas=alphas,
+            sizes=sizes,
+            map_name=map_name,
+            map_type=map_type,
+            dark=dark,
+        )
+        image_files.append("csgo_tmp/{}.png".format(i))
+        f.savefig(image_files[-1], dpi=300, bbox_inches="tight")
+        plt.close()
+    images = []
+    for file in image_files:
+        images.append(imageio.imread(file))
+    imageio.mimsave(filename, images)
+    shutil.rmtree("csgo_tmp/")
+    return True
