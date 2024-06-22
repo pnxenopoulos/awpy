8c1a6084d3247209ac5ce65174c72e1f192d2c86
Peter Xenopoulos <42321130+pnxenopoulos@users.noreply.github.com>
2024-06-22 17:13:05 -0400

    testing release (#339)

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ author = "Peter Xenopoulos"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "2.0.0a7"
+release = "2.0.0a8"
 
 
 # -- General configuration ---------------------------------------------------
