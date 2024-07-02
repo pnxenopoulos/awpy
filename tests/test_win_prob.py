0c1651d033eba823f687be9aafb10d56cc2fa70c
Rory <rorywbush@yahoo.co.uk>
2024-07-02 18:11:39 +0100

    fix tests

--- tests/test_win_prob.py
+++ tests/test_win_prob.py
@@ -1,49 +1,40 @@
 """Tests Win Probability module."""
 
 import pytest
-
 from awpy import Demo
 from awpy.stats import win_probability
 
+@pytest.fixture(scope="class")
+def demo():
+    """
+    Fixture to create and return a Demo object.
+    """
+    return Demo(file='tests/natus-vincere-vs-virtus-pro-m1-overpass.dem')
+
 class TestWinProbability:
-    """Tests the win probability calculations.
-    
-    https://www.hltv.org/matches/2369248/natus-vincere-vs-virtuspro-pgl-cs2-major-copenhagen-2024-europe-rmr-closed-qualifier-a
+    """Test win probability module.
+
+    Teams: Natus Vincere vs Virtus Pro (de_overpass)
+    Event: PGL CS2 Major Copenhagen 2024 Europe RMR Closed Qualifier A (CS2)
+    Source: HLTV
+    Link: https://www.hltv.org/stats/matches/mapstatsid/169189/natus-vincere-vs-virtuspro
     """
 
-    @classmethod
-    def setup_class(cls):
-        """
-        Setup method called before any tests are run.
-        Initializes the Demo object and random weights for testing.
-        """
-        cls.demo = Demo(file='tests/natus-vincere-vs-virtus-pro-m1-overpass.dem')  
 
-
-    @classmethod
-    def teardown_class(cls):
-        """
-        Teardown method called after all tests have run.
-        """
-        cls.demo = None
-
-
-    def test_win_probability_symmetry(self):
-        """Test to ensure P(CT Win) = 1 - P(T Win) at a given tick."""
-        probabilities = win_probability(self.demo, 168200)
+    def test_win_probability_symmetry(self, demo):
+        """Test to ensure P(CT Win) + P(T Win) = 1 at a given tick."""
+        probabilities = win_probability(demo, 168200)
         for prob in probabilities:
-            assert prob["CT_win_probability"] == pytest.approx(1 - prob["T_win_probability"], 0.01)
+            assert prob["CT_win_probability"] + prob["T_win_probability"] == pytest.approx(1.0)
 
-
-    def test_known_ct_sided_situation(self):
+    def test_known_ct_sided_situation(self, demo):
         """Test for a known heavily CT-sided situation."""
-        probabilities = win_probability(self.demo, 168200)
+        probabilities = win_probability(demo, 168200)
         assert probabilities[0]["tick"] == 168200
         assert probabilities[0]["CT_win_probability"] > 0.7
 
-
-    def test_prediction_count_matches_ticks(self):
+    def test_prediction_count_matches_ticks(self, demo):
         """Test that the number of predictions matches the number of ticks passed in."""
         ticks = [166100, 167023, 168200]
-        probabilities = win_probability(self.demo, ticks)
+        probabilities = win_probability(demo, ticks)
         assert len(probabilities) == len(ticks)
