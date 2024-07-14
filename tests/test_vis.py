cef7aabc9e39b6cc76f7a7310f6cfa4bdb4730cd
pnxenopoulos <pnxenopoulos@gmail.com>
2024-07-14 12:00:56 -0400

    a vis module that works

--- tests/test_vis.py
+++ tests/test_vis.py
@@ -19,8 +19,14 @@ class TestVisibility:
 
     def test_basic_visibility(self):
         """Tests basic visibility for de_dust2."""
-        ct_spawn_pos = np.array([15, 2168, -65])
-        t_spawn_pos_1 = np.array([-680, 834, 180])
-        t_spawn_pos_2 = np.array([-1349, -814, 180])
+        ct_spawn_pos = (15, 2168, -65)
+        t_spawn_pos_1 = (-680, 834, 180)
+        t_spawn_pos_2 = (-1349, 814, 180)
+        mid_doors_ct = (-485.90, 1737.51, -60.28)
+        mid_doors_t = (-489.97, 1532.02, -61.08)
+        ct_spawn_towards_b = (-670.19, 2253.08, -56.78)
+        long_a_near_site = (1320.44, 2012.22, 61.44)
         assert is_visible(t_spawn_pos_1, t_spawn_pos_2, "de_dust2")
         assert not is_visible(t_spawn_pos_1, ct_spawn_pos, "de_dust2")
+        assert not is_visible(mid_doors_ct, mid_doors_t, "de_dust2")
+        assert is_visible(ct_spawn_towards_b, long_a_near_site, "de_dust2")
