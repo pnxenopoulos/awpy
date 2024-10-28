0ffa67bf53f55227836a3c74643328ee5b7adff8
ventsiR <ventsipr@gmail.com>
2024-10-28 15:14:09 -0400

    Added plot.utils functions and updated map images
    Added functions in awpy.plot.utils that transform in the opposite direction (i.e. from map pixel coordinates to in-game coordinates) and added documentation indicating direction of transform.
    
    Updated the vertigo map images to show the new layout after the map got updated.

--- awpy/plot/utils.py
+++ awpy/plot/utils.py
@@ -9,7 +9,7 @@ from awpy.data.map_data import MAP_DATA
 def position_transform_axis(
     map_name: str, position: float, axis: Literal["x", "y"]
 ) -> float:
-    """Transforms an X or Y-axis value.
+    """Transforms an X or Y-axis value. CS2 coordinate -> Minimap image pixel value.
 
     Args:
         map_name (str): Map to search
@@ -34,10 +34,40 @@ def position_transform_axis(
     return (start - position) / scale
 
 
+def position_revert_axis(
+    map_name: str, position: float, axis: Literal["x", "y"]
+) -> float:
+    """Reverts an X or Y-axis value. Minimap image pixel value -> CS2 coordinate.
+
+    Args:
+        map_name (str): Map to search
+        position (float): X or Y coordinate
+        axis (str): Either "x" or "y"
+
+    Returns:
+        float: Transformed position
+
+    Raises:
+        ValueError: Raises a ValueError if axis not 'x' or 'y'
+    """
+    axis = axis.lower()
+    if axis not in ["x", "y"]:
+        msg = f"'axis' has to be 'x' or 'y', not {axis}"
+        raise ValueError(msg)
+    start = MAP_DATA[map_name]["pos_" + axis]
+    scale = MAP_DATA[map_name]["scale"]
+
+    if axis == "x":
+        # return (position - start) / scale
+        return position * scale + start
+    # return (start - position) / scale
+    return start - position * scale
+
+
 def position_transform(
     map_name: str, position: tuple[float, float, float]
 ) -> tuple[float, float, float]:
-    """Transforms an single coordinate (X,Y,Z).
+    """Transforms an single coordinate (X,Y,Z). CS2 coordinates -> Minimap image pixel values.
 
     Args:
         map_name (str): Map to transform coordinates.
@@ -49,6 +79,25 @@ def position_transform(
     return (
         position_transform_axis(map_name, position[0], "x"),
         position_transform_axis(map_name, position[1], "y"),
+        position[2],
+    )
+
+
+def position_revert(
+    map_name: str, position: tuple[float, float, float]
+) -> tuple[float, float, float]:
+    """Transforms an single coordinate (X,Y,Z). Minimap image pixel values -> CS2 coordinates.
+
+    Args:
+        map_name (str): Map to transform coordinates.
+        position (tuple): (X,Y,Z) coordinates.
+
+    Returns:
+        Tuple[float, float, float]: Transformed coordinates (X,Y,Z).
+    """
+    return (
+        position_revert_axis(map_name, position[0], "x"),
+        position_revert_axis(map_name, position[1], "y"),
         position[2],
     )
 
