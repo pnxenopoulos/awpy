8d55eba10753e88272b80071be1bb315ff861835
pnxenopoulos <pnxenopoulos@gmail.com>
2025-03-03 00:01:28 -0500

    rc6

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ author = "Peter Xenopoulos"
 # The short X.Y version
 version = "2.0"
 # The full version, including alpha/beta/rc tags
-release = "2.0.0rc5"
+release = "2.0.0rc6"
 
 
 # -- General configuration ---------------------------------------------------
