39873c7eac7f92d51f8eafa2b74ee4ff7205dc24
Jan-Eric Nitschke <47750513+JanEricNitschke@users.noreply.github.com>
2025-05-08 10:40:58 +0200

    Add workaround for dogtown data

--- awpy/data/map_data.py
+++ awpy/data/map_data.py
@@ -42,12 +42,13 @@ class MapData(TypedDict):
 
 def map_data_from_vdf_files(vdf_folder: Path) -> dict[str, MapData]:
     """Generate the map data from a vdf file."""
-    new_map_data: MapData = {}
+    new_map_data: dict[str, MapData] = {}
     for vdf_file in vdf_folder.iterdir():
         # Skip vanity and previews
         if vdf_file.stem.endswith("vanity") or "_preview_" in vdf_file.stem:
             continue
-        parsed_data = vdf.loads(vdf_file.read_text())
+        # Fix for dogtown resource file
+        parsed_data = vdf.loads(vdf_file.read_text().replace("}}", "}\n}"))
         if vdf_file.stem not in parsed_data:
             print(f"Skipping {vdf_file.stem} because the file name is not a valid key.")
             print(f"Keys: {list(parsed_data.keys())}")
